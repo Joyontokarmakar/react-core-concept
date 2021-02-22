8ee703321087d8c2af24fd717a4d539454ee6135
Joyonto Karmakar <41202398+Joyontokarmakar@users.noreply.github.com>
2021-02-23 00:52:11 +0600

    till 7th video

--- src/App.js
+++ src/App.js
@@ -2,24 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const names = ['joyonto Karmakar', 'akm dkfg'];
+  const desgs = ['student', 'job holder']
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done!! Hi there,  <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Person name={names[0]} desg={desgs[0]}></Person>
+        <Person name={names[1]} desg={desgs[1]}></Person>
       </header>
     </div>
   );
+}
+
+function Person(personProperty){
+
+  const personStyle={
+    color:'red',
+    border:'1px solid green', 
+    padding:'10px',
+    margin:'10px'
+  }
+
+  return (
+    <div style={personStyle}>
+      <h1>Name: {personProperty.name} </h1>
+      <p>Designation: {personProperty.desg}</p>
+    </div>
+  )
 }
 
 export default App;
